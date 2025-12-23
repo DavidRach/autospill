d78b8d2d07a36e8077b6d2e609f4b7ab11331ee0
David Rach <drach@som.umaryland.edu>
2025-12-22 21:18:10 -0500

    Edits to Description and Vignette to fall in line with BiocCheck. Added autospill-package.R so that when anyone enters ?autospill they reach a landing page instead of page not found.

---
+++ R/autospill-package.R
@@ -0,0 +1,9 @@
+#' @description
+#' To learn more about how to use autospill, please start by browsing the vignettes:
+#'
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
