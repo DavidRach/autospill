a26d220957236084d2b76bf47d74bc21881b4a82
David Rach <drach@som.umaryland.edu>
2026-01-01 14:23:58 -0500

    Returning a plot, without scatters for the unit test. The existing plotting code is quite unwieldy

--- tests/testthat/test-plot_scatter.R
+++ tests/testthat/test-plot_scatter.R
@@ -35,10 +35,10 @@ test_that("Test that get_compensation_error returns outputs", {
   asp <- the.asp
   
   marker.spillover.zero <- rep( 0, flow.control$marker.n )
-    names( marker.spillover.zero ) <- flow.control$marker
+  names( marker.spillover.zero ) <- flow.control$marker
   
-  marker.proper <- flow.control$sample[1]
-
+  samp <- flow.control$sample[1]
+  marker.proper <- samp
 
   marker.proper.expr.unco <- expr.data.unco[
     which( flow.control$event.sample == samp )[ flow.gate[[ samp ]] ],
@@ -66,31 +66,17 @@ test_that("Test that get_compensation_error returns outputs", {
   marker.spillover.comp.slop <- marker.spillover.zero
   marker.spillover.comp.skew <- marker.spillover.zero
 
-        for ( marker in flow.control$marker )
-        {
-            # get expression data for secondary marker
-
-            if(!is.null(flow.gate)) {
+    marker <- flow.control$marker[1]
+        
                 marker.expr.unco <- expr.data.unco[
                     which( flow.control$event.sample == samp )[
                         flow.gate[[ samp ]] ],
                     marker ]
-            } else {
-                marker.expr.unco <- expr.data.unco[
-                    which( flow.control$event.sample == samp ),
-                    marker ]
-            }
-
-            if(!is.null(flow.gate)) {
+    
                 marker.expr.comp <- expr.data.comp[
                     which( flow.control$event.sample == samp )[
                         flow.gate[[ samp ]] ],
                     marker ]
-            } else {
-                marker.expr.comp <- expr.data.comp[
-                    which( flow.control$event.sample == samp ),
-                    marker ]
-            }
 
             marker.expr.low.unco <- sort( marker.expr.unco )[ expr.trim.n ]
             marker.expr.high.unco <- sort( marker.expr.unco,
@@ -99,8 +85,6 @@ test_that("Test that get_compensation_error returns outputs", {
             marker.expr.low.comp <- sort( marker.expr.comp )[ expr.trim.n ]
             marker.expr.high.comp <- sort( marker.expr.comp,
                 decreasing = TRUE )[ expr.trim.n ]
-
-            # trim expression values for both markers
 
             expr.trim.idx.unco <- which (
                 marker.proper.expr.unco > marker.proper.expr.low.unco &
@@ -124,98 +108,30 @@ test_that("Test that get_compensation_error returns outputs", {
                 expr.trim.idx.comp ]
             marker.expr.trim.comp <- marker.expr.comp[ expr.trim.idx.comp ]
 
-            # fit robust linear model with compensated data
+            marker.spillover.comp.coef[ marker ] <- 1.0
+            marker.spillover.comp.slop[ marker ] <- 1.0
 
-            if ( marker == marker.proper )
-            {
-                marker.spillover.comp.coef[ marker ] <- 1.0
-                marker.spillover.comp.slop[ marker ] <- 1.0
-            }
-            else
-            {
-                spillover.model.result <- fit.robust.linear.model(
-                    marker.proper.expr.trim.comp, marker.expr.trim.comp,
-                    marker.proper, marker, asp )
 
-                marker.spillover.comp.inte[ marker ] <-
-                    spillover.model.result[ 1, 1 ]
-
-                marker.spillover.comp.coef[ marker ] <-
-                    spillover.model.result[ 2, 1 ]
-
-                # get slope and skewness in untransformed scale
-
-                if ( scale.untransformed )
-                {
-                    marker.spillover.comp.slop[ marker ] <-
-                        marker.spillover.comp.coef[ marker ]
-                    marker.spillover.comp.skew[ marker ] <-
-                        skewness( marker.expr.trim.comp )
-                }
-                else
-                {
-                    x.transform.inv <- flow.control$transform.inv[[
-                        flow.control$marker.original[
-                            match( marker, flow.control$marker ) ] ]]
-                    y.transform.inv <- flow.control$transform.inv[[
-                        flow.control$marker.original[
-                            match( marker.proper, flow.control$marker ) ] ]]
-
-                    y1p <- min( marker.proper.expr.trim.comp )
-                    y2p <- max( marker.proper.expr.trim.comp )
-
-                    x1p <- marker.spillover.comp.inte[ marker ] +
-                        marker.spillover.comp.coef[ marker ] * y1p
-                    x2p <- marker.spillover.comp.inte[ marker ] +
-                        marker.spillover.comp.coef[ marker ] * y2p
-
-                    if ( y1p == y2p || x1p == x2p )
-                        marker.spillover.comp.slop[ marker ] <- 0
-                    else
-                    {
-                        y1 <- y.transform.inv( y1p )
-                        y2 <- y.transform.inv( y2p )
-
-                        x1 <- x.transform.inv( x1p )
-                        x2 <- x.transform.inv( x2p )
-
-                        marker.spillover.comp.slop[ marker ] <-
-                            marker.spillover.comp.coef[ marker ] *
-                            ( x2 - x1 ) * ( y2p - y1p ) /
-                            ( ( x2p - x1p ) * ( y2 - y1 ) )
-                    }
-
-                    marker.spillover.comp.skew[ marker ] <-
-                        skewness( x.transform.inv( marker.expr.trim.comp ) )
-                }
-            }
-
-            #if ( plot.figure && ! is.null( flow.control$figure.scatter.dir ) )
-                #plot.scatter(
-                #    expr.data.unco[
-                #        flow.control$event.sample == samp, marker ],
-                #    expr.data.unco[
-                #        flow.control$event.sample == samp, marker.proper ],
-                #    expr.data.comp[
-                #        flow.control$event.sample == samp, marker ],
-                #    expr.data.comp[
-                #        flow.control$event.sample == samp,marker.proper ],
-                #    marker.spillover.unco$inte[ marker.proper, marker ],
-                #    marker.spillover.unco$coef[ marker.proper, marker ],
-                #    marker.spillover.comp.inte[ marker ],
-                #    marker.spillover.comp.coef[ marker ],
-                #    marker.spillover.comp.slop[ marker ],
-                #    range( c( marker.expr.trim.unco, marker.expr.trim.comp ) ),
-                #    range( c( marker.proper.expr.trim.unco,
-                #        marker.proper.expr.trim.comp ) ),
-                #    samp, marker, marker.proper,
-                #    scale.untransformed, figure.label,
-                #    flow.gate, flow.control, asp
-                #)
-          }
-  
-  
-  
+    plot.scatter(
+        expr.data.unco.x=expr.data.unco[flow.control$event.sample == samp, marker ],
+        expr.data.unco.y=expr.data.unco[flow.control$event.sample == samp, marker.proper ],
+        expr.data.comp.x=expr.data.comp[flow.control$event.sample == samp, marker ],
+        expr.data.comp.y=expr.data.comp[flow.control$event.sample == samp,marker.proper ],
+        spillover.unco.inte=marker.spillover.unco$inte[ marker.proper, marker ],
+        spillover.unco.coef=marker.spillover.unco$coef[ marker.proper, marker ],
+        spillover.comp.inte=marker.spillover.comp.inte[ marker ],
+        spillover.comp.coef=marker.spillover.comp.coef[ marker ],
+        spillover.comp.slop=marker.spillover.comp.slop[ marker ],
+        marker.limit.range=range( c( marker.expr.trim.unco, marker.expr.trim.comp ) ),
+        marker.proper.limit.range=range( c( marker.proper.expr.trim.unco,marker.proper.expr.trim.comp ) ),
+        samp=samp,
+        marker=marker,
+        marker.proper=marker.proper,
+        scale.untransformed=TRUE,
+        figure.file.label=TRUE,
+        flow.gate=flow.gate, #NULL
+        flow.control=flow.control,
+        asp=asp)
 
   expect_type(compensation.error, "list")
   expect_named(compensation.error, c("inte", "coef", "slop", "skew"), ignore.order = TRUE)
